2589c1431160cd4290d4cfa0527f72231ebe6fcb
kyubin <kyubin@bespinglobal.com>
2023-06-15 03:52:02 +0900

    build

--- src/config/api.config.js
+++
@@ -1,7 +0,0 @@
-/**
- * app config information
- */
-
-export default {
-  BaseUrl: 'http://localhost:8080',
-};
--- src/plugins/directives.js
+++
@@ -1,7 +0,0 @@
-import focus from '../directives/focus';
-
-export default {
-  install(Vue) {
-    Vue.directive(focus.name, focus);
-  },
-};
